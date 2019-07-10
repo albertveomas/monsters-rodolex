f50013dab79fb88aeecb128036e61b356901e511
Aung Myint Myat <aungmyintmyatiris@gmail.com>
2019-07-10 17:15:21 +0630

    Updates

---
+++ precache-manifest.bef163f30bfaf09027340bd4f95ffea1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16dbd162721620ae167343004e1107c0",
+    "url": "/monsters-rodolex/index.html"
+  },
+  {
+    "revision": "fd1adb0aa285523680a1",
+    "url": "/monsters-rodolex/static/css/main.922c30de.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/monsters-rodolex/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "fd1adb0aa285523680a1",
+    "url": "/monsters-rodolex/static/js/main.ac9f30ac.chunk.js"
+  },
+  {
+    "revision": "a9260227d0878e8e1bc9",
+    "url": "/monsters-rodolex/static/js/runtime~main.6df6c14a.js"
+  }
+]);
